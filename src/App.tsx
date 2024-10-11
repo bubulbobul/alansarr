6b242cd808ce244616012751a088870df04324d7
bubulbobul <windam@live.fr>
2024-10-11 16:00:48 +0200

    feat(ALA-0001): add basic pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home/Home";
+import ContactUs from "./ContactUs/ContactUs";
+import Login from "./Login/Login";
+import SchoolProgram from "./SchoolProgram/SchoolProgram";
+import AboutUs from "./AboutUs/AboutUs";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/schoolprogram" element={<SchoolProgram />} />
+      </Routes>
     </div>
   );
 }
